672f49d65949ccc144ba9bd73315a047260427b0
asrinivas35 <asrinivas5@gatech.edu>
2021-04-03 01:38:33 -0400

    update group card

--- app/components/cards/GroupCard.js
+++ app/components/cards/GroupCard.js
@@ -1,1 +1,43 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import {View, TextInput, Modal, Pressable} from 'react-native';
+import {useAuth} from '../contexts/AuthContext';
+import {useStyles} from '../../contexts/StyleContext';
+
+const GroupCard = ({group}) => {
+    
+    const {currentUser} = useAuth();
+    const {card} = useStyles();
+    const [show, setShow] = useState(false);
+    const [messages, setMessages] = useState([]);
+
+    useEffect(() => {
+        // grab messages from the server
+    }, []);
+
+    const handleShow = () => setShow(true);
+    const handleClose = () => setShow(false);
+
+    return (
+        <View>
+            <View>
+                <Pressable onPress={handleShow}>
+                    <View>
+                        <Text>{group.members}</Text>
+                        <Text>{group.lastMessage}</Text>
+                    </View>
+                </Pressable>
+            </View>
+            <Modal
+                visible={show}
+                onRequestClose={handleClose}
+            >
+                <View>
+                    
+                </View>
+            </Modal>
+        </View>
+    );
+
+};
+
+export default GroupCard;
