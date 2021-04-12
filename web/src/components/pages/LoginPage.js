6cc1dd1da3744a4541bad2afe3b2ecef22be6cf4
Edward <edward.jahoda@gmail.com>
2021-04-12 17:31:53 -0400

    Login screen css

--- web/src/components/pages/LoginPage.js
+++ web/src/components/pages/LoginPage.js
@@ -6,18 +6,25 @@ import Button from 'react-bootstrap/Button'
 
 const LoginPage = () => {
   return (
-    <Container id='LoginContainer'>
+    <Container fluid id='LoginContainer'>
       <div id='Information'>
         <h3>facebook</h3>
-        <h4>Connect with friends and the world around you on Facebook</h4>
+        <h4>Connect with friends and the world around you on Facebook.</h4>
       </div>
-      <div id='LoginContainer'>
+      <div id='LoginFormContainer'>
         <div id='LoginForm'>
-          <input type='text' placeholder='Email or Phone Number' />
-          <input type='text' placeholder='Password' />
-          <Button>Log In</Button>
-          <p>Forgot Password?</p>
-          <Button>Create New Account</Button>
+          <input
+            className='input'
+            type='text'
+            placeholder='Email or Phone Number'
+          />
+          <input className='input' type='text' placeholder='Password' />
+          <Button id='LoginButton'>Log In</Button>
+          <a>Forgot Password?</a>
+          <div id='Line'></div>
+          <Button id='NewAccount' variant='success'>
+            Create New Account
+          </Button>
         </div>
         <div id='LoginText'>
           <p>
