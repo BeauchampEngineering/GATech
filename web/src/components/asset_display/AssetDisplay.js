dcea1beb37b6c4ce3219459c6d9756761c3abdb8
Edward <edward.jahoda@gmail.com>
2021-04-13 20:50:14 -0400

    search bar filter functionality

--- web/src/components/asset_display/AssetDisplay.js
+++ web/src/components/asset_display/AssetDisplay.js
@@ -1,29 +1,86 @@
 import './AssetDisplay.css'
+import { useState } from 'react'
 import Asset from '../asset/Asset'
 
+const assets = [
+  {
+    id: 1,
+    name: 'Pizza Machine',
+    lastModified: '1/13/2020',
+  },
+  {
+    id: 2,
+    name: 'Hot Dog Machine',
+    lastModified: '1/13/2020',
+  },
+  {
+    id: 3,
+    name: 'Roller',
+    lastModified: '1/13/2020',
+  },
+  {
+    id: 4,
+    name: 'Greaser',
+    lastModified: '1/13/2020',
+  },
+  {
+    id: 5,
+    name: 'Air Fryer',
+    lastModified: '1/13/2020',
+  },
+  {
+    id: 6,
+    name: 'Oven',
+    lastModified: '1/13/2020',
+  },
+  {
+    id: 7,
+    name: 'Microwave',
+    lastModified: '1/13/2020',
+  },
+  {
+    id: 8,
+    name: 'Dishwasher',
+    lastModified: '1/13/2020',
+  },
+  {
+    id: 9,
+    name: 'Toaster',
+    lastModified: '1/13/2020',
+  },
+  {
+    id: 10,
+    name: 'Blender',
+    lastModified: '1/13/2020',
+  },
+]
+
 const AssetDisplay = () => {
+  const [searchText, setsearchText] = useState('')
+
+  const searchChanged = (e) => {
+    setsearchText(e.target.value)
+    console.log(e.target.value)
+  }
+
   return (
     <div id='Container'>
       <div id='SearchBarContainer'>
-        <input type='text' placeholder='Search' />
+        <input type='text' placeholder='Search' onChange={searchChanged} />
       </div>
       <div id='Filters'></div>
       <div id='AssetContainer'>
-        <Asset />
-        <Asset />
-        <Asset />
-        <Asset />
-        <Asset />
-        <Asset />
-        <Asset />
-        <Asset />
-        <Asset />
-        <Asset />
-        <Asset />
-        <Asset />
-        <Asset />
-        <Asset />
-        <Asset />
+        {assets
+          .filter((asset) =>
+            asset.name.toLowerCase().includes(searchText.toLowerCase())
+          )
+          .map((asset) => (
+            <Asset
+              key={asset.id}
+              name={asset.name}
+              lastModified={asset.lastModified}
+            />
+          ))}
       </div>
     </div>
   )
