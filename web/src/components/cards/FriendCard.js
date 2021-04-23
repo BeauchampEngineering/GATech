39956a50f2894f8b18667e834ad2ac7b7820cee8
Edward <edward.jahoda@gmail.com>
2021-04-23 17:18:43 -0400

    close button on friend card

--- web/src/components/cards/FriendCard.js
+++ web/src/components/cards/FriendCard.js
@@ -1,11 +1,16 @@
 import './FriendCard.css'
 import edwardImg from '../../resources/edward.jpg'
 import PersonAddIcon from '@material-ui/icons/PersonAdd'
+import CloseIcon from '@material-ui/icons/Close'
 
 const FriendCard = () => {
   return (
     <div className='FriendCard'>
-      <img src={edwardImg} />
+      <div id='ImgAndButton'>
+        <img src={edwardImg} />
+        <CloseIcon id='CloseIcon' />
+      </div>
+
       <h6>Edward Jahoda</h6>
       <button className='AddFriendButton'>
         <div id='ButtonContents'>
