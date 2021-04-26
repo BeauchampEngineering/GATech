8fe0a4a570db196d173e36c40482182266a0d065
Edward <edward.jahoda@gmail.com>
2021-04-26 15:52:36 -0400

    changed asset images to reflect more relevant iamges

--- web/src/components/asset_display/AssetDisplay.js
+++ web/src/components/asset_display/AssetDisplay.js
@@ -3,66 +3,82 @@ import { useState } from 'react'
 import AssetCard from '../cards/AssetCard'
 import Select from 'react-select'
 
+import factoryImg from '../../resources/factory.jpg'
+import formshrinkImg from '../../resources/Formshrink_R_550_g Cropped.jpg'
+import tractorImg from '../../resources/tractor.jpg'
+import multivacImg from '../../resources/Multi-Vac R530 Cropped.jpg'
+import ipaImg from '../../resources/IPA420b Cropped.jpg'
+
 const assets = [
   {
     id: 1,
-    name: 'Pizza Machine',
+    name: 'Factory',
     lastModified: '1/13/2020',
     categories: ['Food'],
+    imgSrc: factoryImg,
   },
   {
     id: 2,
-    name: 'Hot Dog Machine',
+    name: 'Form Shrinking',
     lastModified: '1/13/2020',
     categories: ['Food', 'Manufacturing'],
+    imgSrc: formshrinkImg,
   },
   {
     id: 3,
-    name: 'Roller',
+    name: 'Tractor',
     lastModified: '1/13/2020',
     categories: ['Manufacturing'],
+    imgSrc: tractorImg,
   },
   {
     id: 4,
-    name: 'Greaser',
+    name: 'Multi Vac',
     lastModified: '1/13/2020',
     categories: ['Food', 'Manufacturing'],
+    imgSrc: multivacImg,
   },
   {
     id: 5,
-    name: 'Air Fryer',
+    name: 'IPA',
     lastModified: '1/13/2020',
     categories: ['Food'],
+    imgSrc: ipaImg,
   },
   {
     id: 6,
-    name: 'Oven',
+    name: 'Multi Vac',
     lastModified: '1/13/2020',
     categories: ['Food'],
+    imgSrc: formshrinkImg,
   },
   {
     id: 7,
-    name: 'Microwave',
+    name: 'Multi Vac',
     lastModified: '1/13/2020',
     categories: ['Food'],
+    imgSrc: formshrinkImg,
   },
   {
     id: 8,
-    name: 'Dishwasher',
+    name: 'Multi Vac',
     lastModified: '1/13/2020',
     categories: ['Food'],
+    imgSrc: formshrinkImg,
   },
   {
     id: 9,
-    name: 'Toaster',
+    name: 'Multi Vac',
     lastModified: '1/13/2020',
     categories: ['Food'],
+    imgSrc: formshrinkImg,
   },
   {
     id: 10,
-    name: 'Blender',
+    name: 'Multi Vac',
     lastModified: '1/13/2020',
     categories: ['Food'],
+    imgSrc: formshrinkImg,
   },
 ]
 
@@ -82,33 +98,39 @@ const AssetDisplay = () => {
   }
 
   const renderAssets = () => {
-      return assets.filter((asset) =>
+    return assets
+      .filter((asset) =>
         asset.name.toLowerCase().includes(searchText.toLowerCase())
-      ).filter(categoryFilterFunction).map((asset) => (
+      )
+      .filter(categoryFilterFunction)
+      .map((asset) => (
         <AssetCard
           key={asset.id}
           name={asset.name}
           lastModified={asset.lastModified}
           categories={asset.categories}
+          imgSrc={asset.imgSrc}
         />
-      ));
+      ))
   }
 
   return (
     <div id='AssetDisplayContainer'>
       <div id='SearchBarContainer'>
-        <input type='text' placeholder='Search' onChange={e => setSearchText(e.target.value)} />
+        <input
+          type='text'
+          placeholder='Search'
+          onChange={(e) => setSearchText(e.target.value)}
+        />
       </div>
       <Select
         options={categoryOptions}
         placeholder='Categories'
         isClearable
         className='Selector'
-        onChange={e => e === null ? setFilter(null) : setFilter(e.label)}
+        onChange={(e) => (e === null ? setFilter(null) : setFilter(e.label))}
       />
-      <div id='AssetContainer'>
-        {renderAssets()}
-      </div>
+      <div id='AssetContainer'>{renderAssets()}</div>
     </div>
   )
 }
