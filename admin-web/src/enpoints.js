1d388fb20354a4b37b21723c5cca54f44edd087f
Edward <edward.jahoda@gmail.com>
2021-10-23 18:55:14 -0400

    load all groups in delete/create groups and add new groups to database

--- admin-web/src/enpoints.js
+++ admin-web/src/enpoints.js
@@ -4,6 +4,8 @@ export default {
   CREATE_NEW_USER: baseUrl + 'api/users',
   CREATE_NEW_ASSET: baseUrl + 'api/assets',
   GET_ALL_USERS: baseUrl + 'api/users',
+  CREATE_NEW_GROUP: baseUrl + 'api/groups',
+  GET_ALL_GROUPS: baseUrl + 'api/groups',
   AUTHENTICATE_LOGIN: baseUrl + 'auth/login',
   GET_ASSETS: baseUrl + 'api/assets',
   LOGS: baseUrl + 'api/users/:userId/assets/:assetId/logs',
