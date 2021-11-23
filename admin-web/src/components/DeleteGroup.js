014d689c36b691152d4cb74047eaee3fb2c92e3d
Edward <edward.jahoda@gmail.com>
2021-11-22 22:08:27 -0500

    delete state updating

--- admin-web/src/components/DeleteGroup.js
+++ admin-web/src/components/DeleteGroup.js
@@ -1,13 +1,19 @@
 import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 import endpoints from '../enpoints'
+import { addGroup, groups, setAllGroups, removeGroup } from './state/GroupState'
 import '../component-styles/AddUsersToGroup.css'
 import TextAndButton from './helper/TextAndButton'
 import '../component-styles/DeleteCreateGroup.css'
+import { Autocomplete, TextField } from '@mui/material'
 
 const AddUsersToGroup = () => {
-  const [allGroups, setAllGroups] = useState([])
+  // const [allGroups, setAllGroups] = useState([])
   const [checkBoxes, setCheckBoxes] = useState([])
+  const [selectedGroups, setSelectedGroups] = useState([])
+  const [autoCompleteKey, setAutoCompleteKey] = useState(0) // to clear contents of autocomplete
+
+  var allGroups = groups.use()
 
   useEffect(() => {
     axios
@@ -16,67 +22,51 @@ const AddUsersToGroup = () => {
       .catch((err) => console.log(err))
   }, [])
 
-  const createGroup = (groupName) => {
-    console.log('creating group with name ' + groupName)
-    axios
-      .post(endpoints.CREATE_NEW_GROUP, {
-        name: groupName,
-        userIds: [],
-      })
-      .then((resp) => {
-        console.log('Group created')
-        setAllGroups([...allGroups, resp.data])
-      })
-      .catch((err) => console.log(err))
-  }
-
-  const handleGroupsOnChange = (index) => {
-    let tempCheckBoxes = [...checkBoxes]
-    tempCheckBoxes[index] = !tempCheckBoxes[index]
-    setCheckBoxes(tempCheckBoxes)
-  }
-
   const deleteGroups = () => {
-    const groupsToDelete = allGroups.filter((element, index) => {
-      return checkBoxes[index] === true
-    })
+    const groupsToDelete = new Set(selectedGroups)
 
-    groupsToDelete.forEach((e) => {
-      console.log('todo: remove ' + e.name + ' ' + e.id)
-      axios
-        .delete(endpoints.DELETE_A_GROUP.replace(':groupId', e.id))
-        .then((response) => {
-          console.log('deleted group ' + e.id)
+    allGroups
+      .filter((g) => {
+        return groupsToDelete.has(g.name)
+      })
+      .forEach((g) => {
+        const groupId = g.id
+        axios
+          .delete(endpoints.DELETE_A_GROUP.replace(':groupId', groupId), {
+            groupId: groupId,
+          })
+          .then((response) => {
+            removeGroup(g)
+            setAutoCompleteKey(autoCompleteKey + 1) // clear the autocomplete field
+          })
+          .catch((err) => console.log(err))
+      })
+  }
 
-          setAllGroups(
-            allGroups.filter((g) => {
-              return g.id !== e.id
-            })
-          )
-        })
-        .catch((err) => console.log(err))
-    })
+  function onChange(event, value, details) {
+    setSelectedGroups(value)
   }
 
   return (
     <div>
       <h4>Delete Group(s)</h4>
-      <div className='deleteOrCreateGroups'>
-        <div className='groups'>
-          <div>
-            {allGroups.map((item, index) => (
-              <div className='displayRow alignItems' key={index.toString()}>
-                <input
-                  type='checkbox'
-                  onChange={() => handleGroupsOnChange(index)}
-                />
-                <h5>{item.name}</h5>
-              </div>
-            ))}
-          </div>
-          <button onClick={deleteGroups}>Delete Groups</button>
-        </div>
-      </div>
+      <Autocomplete
+        multiple
+        key={autoCompleteKey}
+        id='user-select'
+        options={allGroups.map((ag) => ag.name)}
+        filterSelectedOptions={true}
+        fullWidth={true}
+        onChange={onChange}
+        renderInput={(params) => <TextField {...params} label='People' />}
+      />
+      <button
+        onClick={() => {
+          deleteGroups()
+        }}
+      >
+        Delete Groups
+      </button>
     </div>
   )
 }
--- admin-web/src/components/state/GroupState.js
+++ admin-web/src/components/state/GroupState.js
@@ -12,3 +12,8 @@ export const addGroup = (newGroup) => {
   console.log('add Group Starts')
   groups.set((value) => [...value, newGroup])
 }
+
+export const removeGroup = (deleteGroup) => {
+  console.log('GS remove group running ')
+  groups.set(groups.get().filter((g) => g !== deleteGroup))
+}
