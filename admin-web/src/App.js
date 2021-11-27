7e7bd6c7cd26bef15811cb1ab6b163a66c023606
Craig Mutsa Nyazema <craignyazema@craigs-air-2.attlocal.net>
2021-11-26 20:37:41 -0500

    Faulting and Fixing elements

--- admin-web/src/App.js
+++ admin-web/src/App.js
@@ -3,12 +3,17 @@ import AddAsset from './components/AddAsset'
 import AddUsersToGroup from './components/AddUsersToGroup'
 import NewUserForm from './components/NewUserForm'
 import DeleteOrCreateGroup from './components/DeleteCreateGroup'
+import FaultAsset from './components/FaultAsset'
+import FixAsset from './components/FixAsset.js'
+
 
 function App() {
   return (
     <div className='App'>
       <NewUserForm />
       <AddAsset />
+      <FaultAsset />
+      <FixAsset />
       <AddUsersToGroup />
       <DeleteOrCreateGroup />
     </div>
