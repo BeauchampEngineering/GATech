f2c0d8edeb73b07d7f77737d7ff86ecf4fd1bea2
Edward <edward.jahoda@gmail.com>
2021-12-01 20:00:26 -0500

    converted HomePage to functional component following standards of functional components

--- app/components/pages/HomePage.js
+++ app/components/pages/HomePage.js
@@ -1,64 +1,58 @@
-import React, { useState,useEffect } from 'react'
-import { View, StyleSheet, Text,FlatList, } from 'react-native'
+import React, { useState, useEffect } from 'react'
+import { View, StyleSheet, Text, FlatList } from 'react-native'
 import endpoints from '../../connections/endpoints'
 import GLOBAL from '../../state/global'
 import LogMessage from '../LogMessage'
-
+import axios from 'axios'
 
 const HomePage = () => {
   const userId = GLOBAL.userId
-  const streamEndpoint = endpoints.STREAM.replace(':userID',userId)
-  const axios = require('axios')
+  const streamEndpoint = endpoints.STREAM.replace(':userID', userId)
+
   const [streamData, setStreamData] = useState([])
   const [isLoading, setLoading] = useState(true)
   const [refresh, setrefresh] = useState(false)
 
-this.state = { 
-    isFetching: false,
-}
   useEffect(() => {
     stream()
-  },[])
+  }, [])
+
   function stream() {
     axios
       .get(streamEndpoint)
       .then((response) => response.data)
       .then((data) => {
         setStreamData(data)
-        console.log(data)
       })
       .catch((err) => console.log(err))
       .finally(() => setLoading(false))
   }
-  onRefresh = () => {
+  const onRefresh = () => {
     stream()
     setrefresh(false)
   }
   return (
     <View style={styles.container}>
-      {console.log(streamData)}
-      <Text>Home page</Text>
       {isLoading ? (
         <Text>Loading</Text>
       ) : (
-      <FlatList
-        style={styles.flatList}
-        //ListHeaderComponent={<SingleAssetHeader image={image} name={name} />}
-        ListFooterComponent={<View style={styles.footer}></View>}
-        data={streamData}
-        refreshing= {refresh}
-        onRefresh={() => this.onRefresh()}
-        renderItem={({ item }) => {
-          return (
-            <LogMessage
-              message={item.message}
-              user={item.userId}
-              date={item.updatedAt}
-            />
-          )
-        }}
-        keyExtractor={(item) => item.id.toString()}
-      ></FlatList>
+        <FlatList
+          style={styles.flatList}
+          ListFooterComponent={<View style={styles.footer}></View>}
+          data={streamData}
+          refreshing={refresh}
+          onRefresh={() => onRefresh()}
+          renderItem={({ item }) => {
+            return (
+              <LogMessage
+                message={item.message}
+                user={item.userId}
+                date={item.updatedAt}
+              />
+            )
+          }}
+          keyExtractor={(item) => item.id.toString()}
+        ></FlatList>
       )}
     </View>
   )
@@ -71,6 +65,7 @@ const styles = StyleSheet.create({
   },
   footer: {
     paddingBottom: 10,
-  },})
+  },
+})
 
 export default HomePage
