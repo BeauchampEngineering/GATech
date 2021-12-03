5aefde4a2baa730eb98ba8d14dfba537a1720d45
Edward <edward.jahoda@gmail.com>
2021-12-03 09:49:02 -0500

    update title for sap form

--- admin-web/src/components/ImportSapForm.js
+++ admin-web/src/components/ImportSapForm.js
@@ -3,39 +3,37 @@ import axios from 'axios'
 import * as XLSX from 'xlsx'
 import endpoints from '../endpoints'
 
-
 const ImportSapForm = () => {
+  const [file, setFile] = useState([])
 
-    const [file, setFile] = useState([])
+  const handleFileUpload = async (e) => {
+    e.preventDefault()
+    const reader = new FileReader()
+    reader.onload = async (e) => {
+      const sap = {}
+      const data = e.target.result
+      const readedData = XLSX.read(data, { type: 'binary', sheetStubs: true })
 
-    const handleFileUpload = async (e) => {
-        e.preventDefault()
-        const reader = new FileReader()
-        reader.onload = async e => {
-            const sap = {}
-            const data = e.target.result
-            const readedData = XLSX.read(data, {type: 'binary', sheetStubs: true})
-
-            for (const wsname of readedData.SheetNames) {
-                const ws = readedData.Sheets[wsname]
-                sap[wsname] = XLSX.utils.sheet_to_json(ws)
-            }
-            await axios.post(endpoints.IMPORT_SAP, sap)
-        }
-        reader.readAsBinaryString(file)
+      for (const wsname of readedData.SheetNames) {
+        const ws = readedData.Sheets[wsname]
+        sap[wsname] = XLSX.utils.sheet_to_json(ws)
+      }
+      await axios.post(endpoints.IMPORT_SAP, sap)
     }
+    reader.readAsBinaryString(file)
+  }
 
-
-    return (
-        <div>
-            <input
-                type="file"
-                accept=".xlsx"
-                onChange={e => setFile(e.target.files[0])}
-            />
-            <button onClick={handleFileUpload}>Upload</button>
-        </div>
-    )
+  return (
+    <div>
+      <h4>Import/Export SAP</h4>
+      <input
+        type='file'
+        accept='.xlsx'
+        onChange={(e) => setFile(e.target.files[0])}
+      />
+      <button onClick={handleFileUpload}>Upload</button>
+    </div>
+  )
 }
 
 export default ImportSapForm
